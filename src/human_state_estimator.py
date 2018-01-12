ab43c66c6c2631b432d8b09ddf781f8f4fddff14
abajcsy <andrea.victoria.b@gmail.com>
2018-01-12 15:35:03 -0800

    added human state estimator for reading from optitrack

---
+++ src/human_state_estimator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python  
+import rospy
+import math
+import tf
+import geometry_msgs.msg
+
+if __name__ == '__main__':
+	rospy.init_node('human_state_estimator')
+
+	listener = tf.TransformListener()
+
+	pose_pub = rospy.Publisher('/human_pose', geometry_msgs.msg.PoseStamped, queue_size=1)
+
+	rate = rospy.Rate(100.0)
+	while not rospy.is_shutdown():
+		try:
+			(trans,rot) = listener.lookupTransform('vicon/hat/hat', '/world', rospy.Time(0))
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			continue
+
+		human_pose = PoseStamped()
+		human_pose.header.frame_id="/world"
+		human_pose.header.stamp = rospy.Time.now()
+		
+		human_pose.pose.position.x = trans[0]
+		human_pose.pose.position.y = trans[1]
+		human_pose.pose.position.z = trans[2]
+
+		pose_pub.publish(human_pose)
+	
+		rate.sleep()
